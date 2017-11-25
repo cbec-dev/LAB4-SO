f76fa16ac3bf225ffb72b8a2de37ad4763c720bb
powei94 <cristobal.becerraf@usach.cl>
2017-11-25 12:44:30 -0300

    funciona ctmmmm

--- src/lab4new.c
+++ src/lab4new.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 {
 
 	int m=3;
-	int n=1;
+	int n=3;
 	int d=0;
 	char fileName[4096];
 	char outName[4096];
@@ -39,6 +39,27 @@ int main(int argc, char **argv)
 	//Se escribe nuevo header
 	writeHeader(outName, header_in, im_height, im_width/m);
 
+	if(n==1) return 0;
+
+	int k = 1;
+	while(k<n)
+	{
+		int **im = readIm(outName);
+		//Se obtiene altura
+		int im_height = getHeight(outName);
+		//Se obtiene ancho
+		int im_width = getWidth(outName);
+		//Se obtiene header
+		int *header_in = malloc(sizeof(int)*54);
+		header_in = getHeader(outName);
+		//Se realiza reduccion
+		reduccion1(im, m, im_height, im_width, d, outName);
+
+		//Se escribe nuevo header
+		writeHeader(outName, header_in, im_height, im_width/m);
+
+		k++;
+	}
 
 
 }
