bbe9be44aaf46fa836e91eee2e54f417cc9d53dd
powei94 <cristobal.becerraf@usach.cl>
2017-11-25 12:34:55 -0300

    funciones

--- src/funciones.c
+++ src/funciones.c
@@ -1,5 +1,5 @@
 
-int **reduccion1(int **im, int m, int filas, int columnas, int d, char *outName)
+void reduccion1(int **im, int m, int filas, int columnas, int d, char *outName)
 {
 	FILE *out;
 	out = fopen(outName, "wb");
@@ -78,5 +78,116 @@ int **reduccion1(int **im, int m, int filas, int columnas, int d, char *outName)
 
 	}
 	fclose(out);
-	return imReducida;
+	//return imReducida;
+}
+
+int **readIm(char *fileName)
+{
+	FILE *in;
+	in = fopen(fileName, "rb");		//Se abre la imagen
+	int im_width;		// Para guardar el ancho
+	int im_height;		// Para guardar el alto
+	int i,j;
+
+
+	//LEER HEADER
+	fseek(in, 18, SEEK_SET);	//SEEK hasta 16 donde está
+								//la información de ancho y alto.
+	fread(&im_width,4,1,in);
+	fread(&im_height,4,1,in);
+
+	fseek(in, 54, SEEK_SET);	//SEEK hasta donde empiezan los valores de los pixeles
+
+
+
+   	int **im = NULL;		//Declarando matriz imagen
+   	im = malloc(sizeof(im)*im_height);
+   	for(i = 0; i<im_height; i++)
+   		im[i] = malloc(sizeof(int)*im_width);
+
+   	for (i = 0; i <im_height; ++i){			//Guardando valores de cada pixel
+		for (j = 0; j < im_width; ++j){
+			fread(&im[i][j],3,1,in);
+		}
+	}
+
+	fclose(in);
+	return im;
+}
+
+int getHeight(char *fileName)
+{
+	FILE *in;
+	in = fopen(fileName, "rb");		//Se abre la imagen
+	int im_width;		// Para guardar el ancho
+	int im_height;		// Para guardar el alto
+
+
+	//LEER HEADER
+	fseek(in, 18, SEEK_SET);	//SEEK hasta 16 donde está
+								//la información de ancho y alto.
+	fread(&im_width,4,1,in);
+	fread(&im_height,4,1,in);
+	
+	fclose(in);
+	return im_height;
+}
+
+int getWidth(char *fileName)
+{
+	FILE *in;
+	in = fopen(fileName, "rb");		//Se abre la imagen
+	int im_width;		// Para guardar el ancho
+	int im_height;		// Para guardar el alto
+
+
+	//LEER HEADER
+	fseek(in, 18, SEEK_SET);	//SEEK hasta 16 donde está
+								//la información de ancho y alto.
+	fread(&im_width,4,1,in);
+	fread(&im_height,4,1,in);
+
+	fclose(in);
+	return im_width;
+}
+
+int *getHeader(char *fileName)
+{
+	FILE *in;
+	in = fopen(fileName, "rb");		//Se abre la imagen
+
+	int *header = malloc(sizeof(int)*54);
+	int i;
+
+
+	fseek(in, 0, SEEK_SET);
+	for (i = 0; i<54; i++){
+		int aux;
+		fread(&aux,1,1,in);
+		header[i] = aux;
+	}
+
+	fclose(in);
+	return header;
+}
+
+void writeHeader(char *fileName, int *header, int height, int width)
+{
+	FILE *out;
+	out = fopen(fileName, "rb+");		//Se abre salida
+	int i;
+
+
+	fseek(out, 0, SEEK_SET);
+	for (i = 0; i<54; i++){
+		int aux;
+		aux = header[i];
+		fwrite(&aux,1,1,out);
+	}
+
+	fseek(out, 18, SEEK_SET);	//SEEK hasta 18 donde está
+								//la información de ancho y alto.
+	fwrite(&width,4,1,out);
+	fwrite(&height,4,1,out);
+	fclose(out);
 }
---
+++ src/lab4new.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <locale.h>
+#include <math.h>
+#include "funciones.h"
+#include "funciones.c"
+
+
+int main(int argc, char **argv)
+{
+
+	int m=3;
+	int n=1;
+	int d=0;
+	char fileName[4096];
+	char outName[4096];
+	strcpy(fileName, "cuadro.bmp");
+	strcpy(outName, "out");
+
+	//Se lee imagen de entrada
+	int **im = readIm(fileName);
+	//Se obtiene altura
+	int im_height = getHeight(fileName);
+	//Se obtiene ancho
+	int im_width = getWidth(fileName);
+
+	//Se obtiene header
+	int *header_in = malloc(sizeof(int)*54);
+	header_in = getHeader(fileName);
+
+	//Se realiza reduccion
+	reduccion1(im, m, im_height, im_width, d, outName);
+
+	//Se escribe nuevo header
+	writeHeader(outName, header_in, im_height, im_width/m);
+
+
+
+}
